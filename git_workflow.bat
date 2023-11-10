ffa80917f1b75cdbd9f5ca52fde942a5c33c746f
Joel_Thomas_Chacko <joel.chacko@tallysolutions.com>
2023-11-10 17:05:45 +0530

    Commenting :)

--- git_workflow.bat
+++ git_workflow.bat
@@ -11,7 +11,7 @@ for /f "usebackq delims=" %%A in (config.txt) do (
     set /a line_count+=1
     if !line_count! equ 1 set "work_dir=%%~A"
     if !line_count! equ 2 set "branch=%%A"
-	if !line_count! equ 2 set "comment=%%A"
+	if !line_count! equ 3 set "comment=%%A"
 )
 
 if not exist "%work_dir%" (

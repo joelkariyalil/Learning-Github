1b33c105e45e2294e3a6016ee0c7df4c0773d4fa
Joel_Thomas_Chacko <joel.chacko@tallysolutions.com>
2023-11-10 16:51:34 +0530

    Adding Those Files

--- git_workflow.bat
+++ git_workflow.bat
@@ -1,54 +1,33 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Read the contents of config1.txt and assign them to variables
 set "work_dir="
 set "branch="
 set /a line_count=0
 
-:: Process config1.txt
 for /f "usebackq delims=" %%A in (config.txt) do (
     set /a line_count+=1
     if !line_count! equ 1 set "work_dir=%%~A"
     if !line_count! equ 2 set "branch=%%A"
 )
 
-echo "Work Directory: !work_dir!"
-echo "Branch: !branch!"
-
-:: Check if directory exists
 if not exist "%work_dir%" (
-    echo Error: Directory does not exist - "%working_directory%"
+    echo Error: Directory does not exist - "%work_dir%"
     pause
-    exit /b 1
+    exit /b
 )
 
-:: Change the working directory
+cd "%work_dir%"
 
-cd /d "%work_dir%"
-echo "in the right directory"
-:: Execute Git commands with the read values
 git stash
 git fetch origin
 git rebase origin
 git stash pop
 git add .
 
-:: Create an AutoIt input dialog to collect the comment message
-:: Save the comment to a temporary file
-echo $input = InputBox("Enter Comment", "Please enter your comment:") > "%temp%\comment.au3"
-echo FileWrite("%temp%\comment.txt", $input) >> "%temp%\comment.au3"
-"C:\Program Files (x86)\AutoIt3\AutoIt3.exe" "%temp%\comment.au3"
-
-:: Read the comment from the temporary file
-set /p message=<"%temp%\comment.txt"
-
-:: Remove the temporary AutoIt script and comment file
-del "%temp%\comment.au3"
-del "%temp%\comment.txt"
+set /p message=Enter Comment: 
 
 git commit -m "%message%"
 git push origin "%branch%"
 
-:: Display a "Press any key to continue..." message
 pause
